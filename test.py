5ae6396c23e5446b419f2c7fb8eb198136819fa3
Joas Wittemans <joas.wittemans@student.ucll.be>
2025-12-15 14:54:01 +0100

    start while loop

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-
-
 import pygame
 
+pygame.init()
 screen_size = (1024, 768)
 
-# Create window with given size
+# # Create window with given size
 pygame.display.set_mode(screen_size)
 # Initialize Pygame
-pygame.init()
 
+# def create_main_surface():
+#     return pygame.display.set_mode(screen_size)
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
