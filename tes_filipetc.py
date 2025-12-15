431006b3b21c8381c3c85945ba3205e9444fd47e
msemen <mohammed.werker@gmail.com>
2025-12-15 20:04:17 +0100

    Update tes_filipetc.py

--- tes_filipetc.py
+++ tes_filipetc.py
@@ -1,9 +1,17 @@
 import pygame
+from pygame.display import *
+
 
 
 screen_size = (1024, 768)
 
-pygame.display.set_mode(screen_size)
+surface = set_mode(screen_size)
+
+def render_frame(surface):
+    circle = pygame.draw.circle(surface, (255,255,255), (500,370) , 12 )
+    flip()
+render_frame(surface)
+    
 # Initialize Pygame
 pygame.init()
 
@@ -14,9 +22,12 @@ while run:
             run = False
 
 pygame.quit()
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 GGGGezedzdz
 kln
 =======
 GGGG
 >>>>>>> Stashed changes
+=======
+>>>>>>> ba40145498c0cd1aba807d9ed0b71794d6a4f2e1
