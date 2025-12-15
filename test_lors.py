2a109f1e44d002858c02e5b2bf347f945d4f9f6b
msemen <mohammed.werker@gmail.com>
2025-12-15 20:04:21 +0100

    Update test_lors.py

--- test_lors.py
+++ test_lors.py
@@ -5,7 +5,7 @@ screen_size = (1024, 768)
 
 screen = pygame.display.set_mode(screen_size)
 
-pygame.display.set_caption("Circle Example")
+pygame.display.set_caption("keys")
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -17,6 +17,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:
+            print("A key was pressed")
 
     screen.fill(BLACK)
 
