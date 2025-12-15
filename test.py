1a9a4ee2aa2752d796d519fbf8e718a98c8b2ea8
JoasWittemans <158040283+JoasWittemans@users.noreply.github.com>
2025-12-15 15:08:36 +0100

    while loop

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-
-
 import pygame
+
 
 screen_size = (1024, 768)
 
@@ -8,3 +7,10 @@ pygame.display.set_mode(screen_size)
 # Initialize Pygame
 pygame.init()
 
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
